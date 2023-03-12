1471444e048b59eaabaee1be71cfb0de74bd778a
Yauhen Pyl <yauhen.pyl@gmail.com>
2023-03-12 18:09:11 +0100

    picker today

--- src/DatePicker.elm
+++ src/DatePicker.elm
@@ -1,7 +1,7 @@
 module DatePicker exposing (Model, Msg, create, getLastSelectedDate, update, view)
 
 import Browser.Dom exposing (Element)
-import Colors exposing (black, gray, white)
+import Colors exposing (black, blue, gray, white)
 import Date exposing (Date, Interval(..), Month, Unit(..), add, day, fromCalendarDate, month, range, weekday, year)
 import Element exposing (Element, alignLeft, alignRight, centerX, column, el, fill, none, padding, px, row, spaceEvenly, spacing, text, width)
 import Element.Background as Background
@@ -172,17 +172,56 @@ dayView today month year =
             , el [ width (px cellWidth), alignRight ] (button [ width fill ] { onPress = Just Next, label = el [ centerX ] (text ">") })
             ]
          , row [ spacing spacingValue, width fill ] ([ "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa" ] |> List.map text |> List.map (el [ centerX ]) |> List.map (el [ width (px cellWidth), Font.bold ]))
-         , row [ spacing spacingValue, width fill ] (generateDaysRow initialDate month)
+         , row [ spacing spacingValue, width fill ] (generateDaysRow today initialDate month)
          ]
-            ++ generateRowsForMonth month (add Days 7 initialDate)
+            ++ generateRowsForMonth today month (add Days 7 initialDate)
         )
+
+
+generateDaysRow : Date -> Date -> Month -> List (Element Msg)
+generateDaysRow today date currentMonth =
+    let
+        txtColor d =
+            if month d == currentMonth && today == d then
+                blue
+
+            else if month d == currentMonth then
+                black
+
+            else
+                gray
+
+        dayBtn d =
+            button [ width (px cellWidth) ] { onPress = Just (SelectDate d), label = el [ centerX, Font.color (txtColor d) ] (text (day d |> String.fromInt)) }
+    in
+    range Day 1 date (add Days 7 date) |> List.map dayBtn
+
+
+generateRowsForMonth : Date -> Month -> Date -> List (Element Msg)
+generateRowsForMonth today monthValue startDate =
+    let
+        internalGenerator dateValue result =
+            if month dateValue == monthValue then
+                row [ spaceEvenly, width fill ] (generateDaysRow today dateValue monthValue) :: internalGenerator (add Days 7 dateValue) result
+
+            else
+                result
+    in
+    internalGenerator startDate []
 
 
 monthView : Date -> Year -> Element Msg
 monthView today year =
     let
+        fontColor m =
+            if m == month today then
+                blue
+
+            else
+                black
+
         monthBtn m =
-            button [ width (px 60) ] { onPress = Just (ShowDayView m year), label = el [ centerX ] (text (monthToShortStr m)) }
+            button [ width (px 60), Font.color (fontColor m) ] { onPress = Just (ShowDayView m year), label = el [ centerX ] (text (monthToShortStr m)) }
     in
     column [ Background.color white, Border.color black, Border.width 1, Border.rounded 5, spacing 20, padding 5 ]
         [ row [ width fill ]
@@ -197,13 +236,20 @@ monthView today year =
 
 
 yearView : Date -> Year -> Element Msg
-yearView today year =
+yearView today yearValue =
     let
+        fontColor y =
+            if y == year today then
+                blue
+
+            else
+                black
+
         startYear =
-            year - 5
+            yearValue - 5
 
         yearBtn y =
-            button [ width (px 60) ] { onPress = Just (ShowMonthView y), label = el [ centerX ] (text (String.fromInt y)) }
+            button [ width (px 60) ] { onPress = Just (ShowMonthView y), label = el [ centerX, Font.color (fontColor y) ] (text (String.fromInt y)) }
     in
     column [ Background.color white, Border.color black, Border.width 1, Border.rounded 5, spacing 20, padding 5 ]
         [ row [ width fill ]
@@ -215,35 +261,6 @@ yearView today year =
         , row [ spacing spacingValue, width fill ] (List.range (startYear + 4) (startYear + 7) |> List.map yearBtn)
         , row [ spacing spacingValue, width fill ] (List.range (startYear + 8) (startYear + 11) |> List.map yearBtn)
         ]
-
-
-generateDaysRow : Date -> Month -> List (Element Msg)
-generateDaysRow date currentMonth =
-    let
-        txtColor d =
-            if month d == currentMonth then
-                black
-
-            else
-                gray
-
-        dayBtn d =
-            button [ width (px cellWidth) ] { onPress = Just (SelectDate d), label = el [ centerX, Font.color (txtColor d) ] (text (day d |> String.fromInt)) }
-    in
-    range Day 1 date (add Days 7 date) |> List.map dayBtn
-
-
-generateRowsForMonth : Month -> Date -> List (Element Msg)
-generateRowsForMonth monthValue startDate =
-    let
-        internalGenerator dateValue result =
-            if month dateValue == monthValue then
-                row [ spaceEvenly, width fill ] (generateDaysRow dateValue monthValue) :: internalGenerator (add Days 7 dateValue) result
-
-            else
-                result
-    in
-    internalGenerator startDate []
 
 
 deltaBasedOnWeekDay : Weekday -> Int
